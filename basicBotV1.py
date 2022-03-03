fadcf5e530362124062b15813c62337553c0b103
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2022-03-03 07:29:04 -0500

    Complete Overhaul of bot

--- basicBotV1.py
+++ basicBotV1.py
@@ -1,32 +1,82 @@
 import time
 import itertools
 from API import ethPriceReport
+import csv
 
-print("Check Check")
-
-simulationETH = 1
-simulationUSD = 1000
-amountToConvert = 0.25
 ethPrice = ethPriceReport()['priceETH']
-
 seconds = time.time()
 
-def liquidate():
-    simulationETH -= amountToConvert
-    simulationUSD += amountToConvert * ethPrice
-    print("Currently(" + time.ctime(seconds) + "), ETH reserves are at " + str(simulationETH) + " and USDC reserves are at " + str(simulationUSD))
 
+
+def logger(sec, eth, usd, percentChange):
+    with open('priceDataCsv.csv', 'w', newline='') as f:
+        fieldnames = ['time', 'ethReserve', 'usdReserve']
+        thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+
+        currentTime = time.ctime(sec)
+
+        thewriter.writeheader()
+        thewriter.writerow({'time' : currentTime,'ethReserve' : eth, 'usdReserve' : usd)
+
+def reader():
+    with open('priceDataCsv.csv', 'r') as f:
+        reader = csv.DictReader(f)
+
+        allLines = list(reader)
+        latestLine = allLines[-1]
+
+        ethReserves = latestLine['ethReserve']
+        usdReserves = latestLine['usdReserve']
+
+        returnDictionary = {
+            'ethRes' : ethReserves,
+            'usdRes' : usdReserves
+        }
+
+        return returnDictionary
+
+
+
+
+
+
+
+
+
+
+def liquidate(seconds):
+    simulationETH = reader()['ethRes']
+    simulationUSD = reader()['usdRes']
+
+    simulationETH -= amountToConvert
+    simulationUSD += (amountToConvert * ethPrice)
+
+    time.time()
+    secs = time.ctime(seconds)
+    logger(secs, simulationETH, simulationUSD)
 
 
 def buyDip():
+    simulationETH = reader()['ethRes']
+    simulationUSD = reader()['usdRes']
+
     simulationETH += amountToConvert
     simulationUSD -= (amountToConvert * ethPrice)
-    print("Currently(" + time.ctime(seconds) + "), ETH reserves are at " + str(simulationETH) + " and USDC reserves are at " + str(simulationUSD))
+
+    time.time()
+    secs = time.ctime(seconds)
+    logger(seconds, simulationETH, simulationUSD)
+
+
 
 print("Check Check")
-
+'''
 for x in itertools.repeat([]):
-    if (ethPriceReport()['percentChangeHRLY'] > 0.02):
-        liquidate()
-    elif (ethPriceReport()['percentChangeHRLY'] < 0.02):
-        buyDip()
+    if (ethPriceReport()['percentChangeHRLY'] > 0.2):
+
+        seconds = time.time()
+    elif (ethPriceReport()['percentChangeHRLY'] < -0.2):
+
+        seconds = time.time()
+    time.sleep(120)
+'''
