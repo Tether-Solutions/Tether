a3745c8326e4f9cda07c50b0d315e898db91ccec
Yuvanesh-ux <68208096+Yuvanesh-ux@users.noreply.github.com>
2022-07-28 22:23:26 -0400

    price report consolidation

--- tether/messages.py
+++ tether/messages.py
@@ -31,7 +31,7 @@ class MessageSender:
             email.login(sender_email, email_password)
             email.sendmail(sender_email, receiver_email, email_message)
 
-    def price_report_down(self, number: str):
+    def price_report(self, number: str):
         """Packages current Etherium info in a sms format
 
         Args:
@@ -39,35 +39,21 @@ class MessageSender:
 
         """
         # Preprocessing
-        ether_price = round(self.ether_data.eth_price_report()['price_eth'], 4)
-        eth_percent_change_hourly = round(self.ether_data['percent_change_hourly'] / 100, 4)
 
-        # Message elements
-        percentage_modifier = 1 + abs(eth_percent_change_hourly)
-        previous_hour_price = round(percentage_modifier * ether_price, 4)
-
-        message = f"Ethereum has gone down from {previous_hour_price} to {ether_price} in the last hour ({eth_percent_change_hourly}%)"
-
-        print("\nSending...")
-        self.send_sms_via_email(number, message, self.provider, self.sender_credentials)
-        print("\nSent")
-
-    def price_report_up(self, number: str):
-        """Packages current Etherium info in a sms format
-
-        Args:
-            number(str): The target phone number of the sms message
-
-        """
-        # Preprocessing
-        ether_price = round(self.ether_data.eth_price_report()['price_eth'], 4)
+        ether_price = round(self.ether_data.eth_price_report()['price_eth'], 2)
         eth_percent_change_hourly = round(self.ether_data.eth_price_report()['percent_change_hourly'] / 100, 4)
 
-        # Message elements
-        percentage_modifier = 1 - abs(eth_percent_change_hourly)
-        previous_hour_price = round(percentage_modifier * ether_price, 4)
+        if eth_percent_change_hourly > 0:
+            # Message elements
+            percentage_modifier = 1 - abs(eth_percent_change_hourly)
+            previous_hour_price = round(percentage_modifier * ether_price, 2)
 
-        message = f"Ethereum has gone up from {previous_hour_price} to {ether_price} in the last hour ({eth_percent_change_hourly}%)"
+            message = f"Ethereum is up!\n from ${previous_hour_price} to ${ether_price} in the last hour ({eth_percent_change_hourly * 100}%)"
+        else:
+            percentage_modifier = 1 + abs(eth_percent_change_hourly)
+            previous_hour_price = round(percentage_modifier * ether_price, 2)
+
+            message = f"Ethereum is down!\n from ${previous_hour_price} to ${ether_price} in the last hour ({eth_percent_change_hourly * 100}%)"
 
         print("\nSending...")
         self.send_sms_via_email(number, message, self.provider, self.sender_credentials)
@@ -76,4 +62,4 @@ class MessageSender:
 
 if __name__ == "__main__":
     sender = MessageSender('T-Mobile')
-    sender.price_report_up('8045479964')
+    sender.price_report('8045479964')
